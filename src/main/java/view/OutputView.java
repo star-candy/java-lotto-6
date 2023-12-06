315f82e6ce238da7f0d33be95fc8520c7b23b0b1
star-candy <el0710@naver.com>
2023-12-07 00:33:52 +0900

    feat(lotto): 로또 구매 갯수 출력 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,9 @@
 package view;
 
+import constants.Description;
 public class OutputView {
+    public void lottoCount(int payment) {
+        int lottoCount = payment / 1000;
+        System.out.printf(Description.LOTTO_COUNT.getMessage(), lottoCount);
+    }
 }
